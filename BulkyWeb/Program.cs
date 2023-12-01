bb9dc89a80e759d62c912ad8fd29b68b0243f1b5
Kartikey Pathak <kartikeypathak007@gmail.com>
2023-12-02 00:47:11 +0530

    Displaying objected on webapp Closing...

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -9,7 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-//builder.Services.AddDbContext<ApplicationDbContext>();
 builder.Services.AddDbContext<ApplicationDbContext>(options=> 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
