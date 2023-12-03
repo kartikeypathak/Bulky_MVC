3f411aa14a077228fed42b2969b67b05c1e857b1
Kartikey Pathak <kartikeypathak007@gmail.com>
2023-12-03 21:55:59 +0530

    Changes mad yesterday. Of frontend.

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -16,5 +16,18 @@ namespace BulkyWeb.Controllers
             List<Category> ObjCategoryList = _db.Categories.ToList();
             return View(ObjCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
